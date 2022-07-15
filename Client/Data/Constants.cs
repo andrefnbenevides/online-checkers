47b2e71e9e397938be9b030aea5c44d0b8087d45
andrefnbenevides <andre.rhagnar93@gmail.com>
2022-07-16 00:09:02 +0100

    rearrange file system

---
+++ Client/Data/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace OnlineCheckers.Client.Data
+{
+    public class Constants
+    {
+        public const string WHITE_CHECKER_COLOR_NAME = "white";
+        public const string BLACK_CHECKER_COLOR_NAME = "black";
+    }
+}
