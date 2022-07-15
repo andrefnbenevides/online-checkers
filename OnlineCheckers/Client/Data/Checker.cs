af3af59dc51c9406dece21d709c0c3a8aa3d285a
andrefnbenevides <andre.rhagnar93@gmail.com>
2022-07-15 14:54:32 +0100

    initial commit

---
+++ OnlineCheckers/Client/Data/Checker.cs
@@ -0,0 +1,15 @@
+﻿namespace OnlineCheckers.Client.Data
+{
+    public class Checker
+    {
+        public string Color { get; set; }
+        public int Row { get; set; }
+        public int Column { get; set; }
+        public CheckerDirection Diretion { get; set; }
+    }
+
+    public enum CheckerDirection
+    {
+        Up, Down, Both
+    }
+}
